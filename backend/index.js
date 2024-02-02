c84c2bea95468c1f59c104c814e09c4c2b80e1da
Patrikator17 <patkarpratik42>
2024-02-02 22:05:20 +0530

    Connect to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose';
 
 const app = express()
+
+mongoose.connect("mongodb+srv://pratikgauth:pratikgauth@mern.dofdkic.mongodb.net/?retryWrites=true&w=majority")
+.then(() =>{
+    console.log('Connected to MongoDB');
+})
+.catch((err) => {
+    console.log(err);
+})
 
 app.listen(3000, ()=>{
     console.log('Server running on port 3000');
 })
+
+// pratikgauth
