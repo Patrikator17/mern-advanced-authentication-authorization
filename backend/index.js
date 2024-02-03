465e4205ef11b99f38b3f998485d954b18d94b59
Patrikator17 <patkarpratik42>
2024-02-03 11:18:39 +0530

    create sign up api bcrypt

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import userRouter from './routes/user.routes.js'
+import authRoute from './routes/user.auth.js'
 
 const app = express()
 
+app.use(express.json())
 mongoose.connect("mongodb+srv://pratikgauth:pratikgauth@mern.dofdkic.mongodb.net/?retryWrites=true&w=majority")
 .then(() =>{
     console.log('Connected to MongoDB');
@@ -17,4 +19,5 @@ app.listen(3000, ()=>{
 })
 
 app.use('/user', userRouter)
+app.use('/auth', authRoute)
 // pratikgauth
